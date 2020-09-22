3efe41e2af22517b6d6ebe8246bd629980f36072
lalosaid <eduardo.said.mndz@gmail.com>
2020-09-22 14:56:46 -0500

    Add key functionality

---
+++ app/models/stock.rb
@@ -0,0 +1,12 @@
+class Stock < ApplicationRecord
+
+    # Using self makes the method callable from inside the class
+    def self.new_lookup(ticker_symbol)
+        client = IEX::Api::Client.new(
+            publishable_token: Rails.application.credentials.iex_client[:sandbox_api_key],
+            endpoint: 'https://sandbox.iexapis.com/v1'
+        )
+        client.price(ticker_symbol)
+    end
+
+end
